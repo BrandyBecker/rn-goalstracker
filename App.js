f3d5f74d1db57bc26486769417a44836fa56e693
BrandyBecker <brandybeckerfsw@gmail.com>
2020-04-08 15:16:16 -0400

    Flexed the input & button beside each other

--- App.js
+++ App.js
@@ -3,16 +3,16 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 30}}>
+    <View style={{padding: 50}}>
     {/* Main App View */}
-      <View>
+      <View style={{flexDirection: 'row'}}>
        {/* Text Input View */}
-        <TextInput/>
+        <TextInput placeholder="What is your goal ?" style={{borderColor: 'black', borderWidth: 1, padding: 10}}/>
        {/* Button View */}
        <Button title="Add"/>
       </View>
       <View>
-       
+    
       </View>
     </View>
   );
