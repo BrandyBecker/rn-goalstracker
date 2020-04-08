5ee867dfad99692c0c45959c68d218baa4b90c63
BrandyBecker <brandybeckerfsw@gmail.com>
2020-04-08 14:52:03 -0400

    Fresh Start

--- App.js
+++ App.js
@@ -3,17 +3,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+    <View>
+    {/* Main App View */}
+      <View>
+       {/* Text Input View */}
+        
+      </View>
+      <View>
+       {/* Button View */}
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
