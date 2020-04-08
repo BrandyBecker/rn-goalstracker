f9c2a9c1cdffcd2bae9360ff0bdd6bb41ea61291
BrandyBecker <brandybeckerfsw@gmail.com>
2020-04-08 15:23:38 -0400

    Aligned and Justified items, added a width to the Input

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <View style={{padding: 50}}>
     {/* Main App View */}
-      <View style={{flexDirection: 'row'}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
        {/* Text Input View */}
-        <TextInput placeholder="What is your goal ?" style={{borderColor: 'black', borderWidth: 1, padding: 10}}/>
+        <TextInput placeholder="What is your goal ?" style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
        {/* Button View */}
        <Button title="Add"/>
       </View>
